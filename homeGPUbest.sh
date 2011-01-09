14a974287c0bd94514a9659ff36265cd4e55ffb1
Dwight Bell <dbelll@yahoo.com>
2011-01-09 05:44:11 -0500

    homeGPUbest.sh

---
+++ homeGPUbest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Baseline comparison of GPU and CPU results
+#
+#   no arguments
+#
+_location="./bin/linux/release"
+_setup="--SEED=1001 --BOARD_SIZE=5007 --NUM_PIECES=2 --MAX_TURNS=5"
+_agent="--NUM_HIDDEN=4 --NUM_AGENTS=64 --NUM_OPPONENTS=32"
+_learn="--NUM_SESSIONS=8000 --SEGS_PER_SESSION=2 --EPISODE_LENGTH=5 --WARMUP_LENGTH=0 --BENCHMARK_GAMES=400 --BENCHMARK_FREQ=400 --BENCHMARK_OPS=32 --BEGIN_USING_BEST_OPS=1000000 --DETERMINE_BEST_OP_FREQ=100 --ICHAMP=1"
+_params="--ALPHA=0.20 --EPSILON=0.00 --GAMMA=0.95 --LAMBDA=0.75"
+_run="--GPU"
+
+$_location/fastrack $_setup $_agent $_learn $_params $_run
+
