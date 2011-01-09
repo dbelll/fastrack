0175a09dc83a82c5911ee840ef4dbce0298fe7be
Dwight Bell <dbelll@yahoo.com>
2011-01-09 09:24:21 -0500

    increase max opponents

---
+++ resGPU2test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Baseline comparison of GPU and CPU results
+#
+#   no arguments
+#
+_location="./bin/linux/release"
+_setup="--SEED=1001 --BOARD_SIZE=5007 --NUM_PIECES=2 --MAX_TURNS=5"
+_agent="--NUM_HIDDEN=4 --NUM_AGENTS=64 --NUM_OPPONENTS=32"
+_learn="--NUM_SESSIONS=4000 --SEGS_PER_SESSION=1 --EPISODE_LENGTH=5 --BENCHMARK_GAMES=400 --BENCHMARK_FREQ=200 --ICHAMP=1"
+_params="--ALPHA=0.20 --EPSILON=0.00 --GAMMA=0.95 --LAMBDA=0.75"
+_run="--GPU"
+
+$_location/fastrack $_setup $_agent $_learn $_params $_run
